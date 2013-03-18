bcd74a1f237f8f076e315a500903eb5d652d57ca
dargani123+bheithaus <dargani123+bheithaus@appacademy.io>
2013-03-18 15:28:39 -0700

    flagging and basic UI is in

--- minesweeper.rb
+++ minesweeper.rb
@@ -17,32 +17,31 @@ class Minesweeper
     @board = Board.new
   end
 
-
   def play
-    ##loop
-      ##get input
-      ##valid input?
-
-      ##call click on that square
-    #repeat
-    loop do
+    game_over = 0
+    until game_over != 0
       show
       flag, coord = get_input
       if flag
-        @board.flag( coord )
+        game_over = @board.flag( coord )
       else
-        @board.click( coord )
+        game_over = @board.click( coord )
       end
     end
-
-
+    show
+    case game_over
+    when -1
+      puts "you got blown up :("
+    when 1
+      puts "you won!"
+    end
   end
 
   def get_input  ##  doesn't test valid?
     flag = false
     puts "enter coordinate on the board to click, enter 'f' to flag"
     input = gets.chomp
-    if input == "f"
+    if input == "f" || input == "f "
       flag = true
       puts "enter coordinate on the board to flag"
       input = gets.chomp
@@ -53,12 +52,8 @@ class Minesweeper
     [flag, coord]
   end
 
-
-
-
-
-
   def show
+    puts "number of mines: #{@board.bombs} | flags: #{@board.flags}"
     @board.board_display.each do |row|
       row.each do |square|
         print "#{square} "
@@ -66,7 +61,6 @@ class Minesweeper
       puts
     end
   end
-
 
   # def show_cheat
 #     @board.each do |row|
@@ -90,6 +84,8 @@ end
 
 class Board
 
+  attr_reader :flags
+
   XADD = [-1,0,1,1,1,0,-1,-1]
   YADD = [1,1,1,0,-1,-1,-1,0]
 
@@ -99,30 +95,57 @@ class Board
     #  [sq,sq,sq],
     #  [sq,sq,sq]]
     @board = Array.new(9) {[Square.new(false, 0)]*9}
-    bomb_coords = create_bombs(number_of_bombs)
-    make_board(bomb_coords)
+    @flags = 0
+    @bomb_coords = create_bombs(number_of_bombs)
+    make_board(@bomb_coords)
     puts "debug:"
     show_debug
     puts "the one"
   end
 
-  def flag
+  def bombs
+    @bomb_coords.size
+  end
 
+  def won?
+    return false unless @bomb_coords.size == @flags
+
+    @bomb_coords.each do |coord|
+      return false unless @board[coord[0]][coord[1]].flagged
+    end
+    true
+  end
+
+  def flag (coord)
+    square = @board[ coord[0] ][ coord[1] ]
+    return 0 if square.revealed
+    if square.flagged
+      @flags -= 1
+    else
+      @flags += 1
+    end
+    square.flagged = !square.flagged
+    return 1 if won?
+    0
   end
 
   def click(coord)
     square = @board[ coord[0] ][ coord[1] ]
     if square.flagged == true
       square.flagged = false
+      @flags -= 1
+      return 1 if won?
     else
         if square.bomb
-          return "bomb"
+          square.revealed = true
+          return -1
         elsif square.number == 0
           reveal(coord)
         else
           square.revealed = true
         end
     end
+    0
   end
 
   def reveal(coord)
@@ -130,17 +153,16 @@ class Board
     #
     square = @board[coord[0]][coord[1]]
 
-    if square.number > 0
-      square.revealed = true
-    elsif square.bomb || square.flagged || square.revealed
+    if square.flagged || square.revealed || square.bomb
       return
+    elsif square.number > 0
+       square.revealed = true
     else #if number == 0       ##refactor , same as count_bombs
       square.revealed = true
       get_neighbors(coord).each {|neighbor| reveal(neighbor)}
     end
 
   end
-
 
   def make_board(bomb_coords)
     ## add bombs
@@ -195,7 +217,11 @@ class Board
             board_display[i][j] = "_"
           end
         else
-          board_display[i][j] =  "*"
+          if @board[i][j].flagged
+           board_display[i][j] =  "f"
+          else
+           board_display[i][j] =  "*"
+          end
         end
       end
     end
